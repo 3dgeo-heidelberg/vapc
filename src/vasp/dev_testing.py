5f0e2e9abd44790e1e11a58521ec0566023b316e
Ronald Tabernig <ronald.tabernig@gmail.com>
2024-05-02 16:26:28 +0200

    testing added

--- src/vasp/dev_testing.py
+++ src/vasp/dev_testing.py
@@ -1,6 +1,9 @@
 from vasp import VASP
 from data_handler import DATA_HANDLER
 import os
+import laspy
+import numpy as np
+
 
 base_folder = os.path.abspath(os.curdir)
 test_folder = os.path.join(base_folder,"test_data")
@@ -55,8 +58,20 @@ vasp.filter_attributes( filter_attribute = "point_count",
                         min_max_eq = "min",
                         filter_value = 2,
                         )
+
 vasp.compute_requested_statistics_per_attributes()
 vasp.compute_requested_attributes()
 vasp.reduce_to_voxels()
 dh.df = vasp.df
+cols = dh.df.columns
 dh.save_as_las(test_out)
+
+with laspy.open(test_out,"r") as lf_t:
+    las_t = lf_t.read()
+with laspy.open(test_ref,"r") as lf_r:
+    las_r = lf_r.read()
+
+for col in cols:
+    errors = np.unique(np.round(las_r[col],5) == np.round(las_t[col],5))
+    if False in errors:
+        print("Error for attribute: %s"%col)
