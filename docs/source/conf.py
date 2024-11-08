cb860ba786c788e50f7d8bf6d841995dcccd6707
tabernig <ronald.tabernig@gmail.com>
2024-11-08 23:56:44 +0100

    doc config

---
+++ docs/source/conf.py
@@ -0,0 +1,57 @@
+"""
+cd vasp/docs
+make.bat html
+start build/html/index.html
+"""
+
+
+import os
+import vasp
+import subprocess
+import sys
+
+
+
+sys.path.insert(0, os.path.abspath("../../src"))
+os.environ["XDG_DATA_DIRS"] = os.path.abspath("../tests/data")
+
+# Configuration file for the Sphinx documentation builder.
+
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "vasp"
+copyright = "2024, 3DGeo Research Group, Heidelberg University"
+author = "Ronald Tabernig"
+release = vasp.get_version()
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',  # For NumPy and Google style docstrings
+    'sphinx_autodoc_typehints',  # For type hints
+    'sphinx.ext.viewcode',  # To include source code
+    "nbsphinx",
+    "nbsphinx_link",
+    "sphinx_mdinclude",
+    "sphinx_rtd_theme"
+]
+
+templates_path = []
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = []
+
+napoleon_google_docstring = False
+napoleon_numpy_docstring = True
