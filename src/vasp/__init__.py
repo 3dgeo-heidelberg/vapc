f5d9a382718e9657ce4a1155028b24a419a034a8
tabernig <ronald.tabernig@gmail.com>
2024-11-08 23:55:19 +0100

    update init

--- src/vasp/__init__.py
+++ src/vasp/__init__.py
@@ -2,4 +2,4 @@ from vasp.vasp import VASP
 from vasp.data_handler import DATA_HANDLER
 from vasp.utilities import *
 from vasp.vasp_tools import use_tool
-from vasp.main import do_vasp_on_files
+from vasp.main import do_vasp_on_files, main
