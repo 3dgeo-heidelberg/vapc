c3406ff0acd4d13af6a4e8ba6f7ac9a3a13c6399
tabernig <ronald.tabernig@gmail.com>
2024-11-08 23:49:38 +0100

    add main for tools

--- src/vasp/main.py
+++ src/vasp/main.py
@@ -4,8 +4,9 @@ import datetime
 import json
 import argparse
 from vasp.utilities import *
+# from vasp import __version__
 
-VASP_VERSION = "0.0.0.1"
+
 
 @trace
 @timeit
@@ -15,14 +16,67 @@ def do_vasp_on_files(file,
                 vasp_command,
                 tile = False, 
                 reduce_to = "closest_to_center_of_gravity",
-                save_as = ".laz"):
+                save_as = ".laz",
+                doc = True):
+    """
+    Executes VASP operations on LAS/LAZ files based on the provided configuration.
+
+    This function handles the voxelization process, optional tiling for large datasets,
+    applies VASP tools, and saves the processed data in the desired format. It also
+    manages temporary files and directories used during processing.
+
+    Parameters
+    ----------
+    file : str or list of str
+        Path to a single LAS/LAZ file or a list of paths to LAS/LAZ files to be processed.
+    out_dir : str
+        Directory where output files and configurations will be saved.
+    voxel_size : float
+        Defines the size of each voxel for processing.
+    vasp_command : dict
+        Dictionary containing VASP command configurations, including the tool to use
+        and any additional arguments.
+    tile : bool, optional
+        If True, enables tiling for processing large datasets by partitioning into smaller tiles.
+        Defaults to False.
+    reduce_to : str, optional
+        Specifies the method to reduce the DataFrame to one value per voxel. Options include:
+        "closest_to_center_of_gravity", "center_of_voxel", "center_of_gravity","corner_of_voxel".
+        Defaults to "closest_to_center_of_gravity".
+    save_as : str, optional
+        Specifies the output file format. Options include ".laz", ".las", ".ply".
+        Defaults to ".laz".
+    doc : bool, optional
+        If True, saves the processing configuration as a JSON file for documentation purposes.
+        Defaults to True.
+
+    Returns
+    -------
+    bool
+        Returns True if processing is successful.
+
+    Raises
+    ------
+    FileNotFoundError
+        If input files are not found.
+    ValueError
+        If invalid parameters are provided.
+    KeyError
+        If required keys are missing in the vasp_command dictionary.
+
+    Notes
+    -----
+    - When tiling is enabled, temporary tiles are created in a subdirectory within out_dir.
+    - After processing, temporary files and directories are cleaned up to save space.
+    - The configuration used for processing is saved as a JSON file if `doc` is True.
+    """
     #get_timestamp_for_output
     now = datetime.datetime.now()
     timestamp = now.strftime("%Y_%m_%d_%H-%M-%S")
 
     #document settings
     config = vasp_command
-    config["vasp_version"] = VASP_VERSION
+    config["vasp_version"] = get_version()
     config["file"] = file
     config["out_dir"] = out_dir
     config["voxel_size"] = voxel_size
@@ -82,10 +136,11 @@ def do_vasp_on_files(file,
                  reduce_to = vasp_command["reduce_to"])
 
     #save configuration for documentation
-    with open(outconfig, 'w') as cfg:
-        json.dump(config, 
-                  cfg,
-                  indent=4)
+    if doc:
+        with open(outconfig, 'w') as cfg:
+            json.dump(config, 
+                    cfg,
+                    indent=4)
 
 
     if save_as == ".laz":
@@ -105,15 +160,53 @@ def do_vasp_on_files(file,
 
 
 def parse_args():
+    """
+    Parses command-line arguments for the VASP command.
+
+    Returns
+    -------
+    argparse.Namespace
+        An object containing the parsed command-line arguments.
+    """
     parser = argparse.ArgumentParser(description="Use VASP.")
     parser.add_argument("config_file", help="Configuration of the VASP command")
     return parser.parse_args()
 
 def load_config(config_file):
+    """
+    Loads the VASP command configuration from a JSON file.
+
+    Parameters
+    ----------
+    config_file : str
+        Path to the JSON configuration file.
+
+    Returns
+    -------
+    dict
+        A dictionary containing the VASP command configurations.
+    """
     with open(config_file, 'r') as file:
         return json.load(file)
 
 def main():
+    """
+    The main function that orchestrates the VASP processing workflow.
+
+    This function parses command-line arguments, loads the configuration,
+    and executes the VASP operations on the specified files.
+
+    Raises
+    ------
+    Exception
+        If any step in the processing workflow fails.
+
+    Examples
+    --------
+    To run VASP with a configuration file:
+    
+    >>> python run.py config.json
+    """
     # Get input 
     args = parse_args()
 
